8b196a73ab0235c3c8f7e12995ed846bdb8b34b9
gonchesan <gmsanchezgaray@gmail.com>
2025-09-16 17:31:33 -0300

    [ADD] new interfaces & new functionaly to make a revision in base of a suggestion selected

--- src/components/Suggestions.tsx
+++ src/components/Suggestions.tsx
@@ -3,45 +3,37 @@ import React, { useEffect } from 'react';
 //SuggestionCard selectable
 import useLocalStorage from '@/hooks/useLocalStorage.tsx';
 import { getListTopicsPrompt } from '@/utils/prompts';
-import type { SuggestionCardProps, SuggestionProps } from '@/types';
+import type {
+  SuggestionCardProps,
+  SuggestionProps,
+  WritingPrompts,
+} from '@/types';
 import gemini from '@/gemini';
 import { generateGuid } from '@/utils/guuid';
+import { extractJsonFromString } from '@/utils/json';
 
 const SuggestionList: React.FC<{
   onChangeSuggestion: React.Dispatch<React.SetStateAction<string | null>>;
 }> = ({ onChangeSuggestion, selectedSuggestion }) => {
-  const [suggestions, setSuggestions] = useLocalStorage('suggestions', []);
+  const [suggestions, setSuggestions] = useLocalStorage<SuggestionProps[]>(
+    'suggestions',
+    [],
+  );
 
   async function generateContent(prompt: string) {
     const result = await gemini.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
-    //AUX methods
-    const jsonRegex = /```json\n([\s\S]*?)\n```/;
-    const match = text.match(jsonRegex);
-
-    if (match && match[1]) {
-      // The first capture group (`match[1]`) contains the pure JSON string.
-      const jsonString = match[1];
-
-      try {
-        // 4. Parse the JSON string into a JavaScript object.
-        const data = JSON.parse(jsonString);
-        let suggestionFormatted = [];
-        if (data?.writing_prompts.length) {
-          suggestionFormatted = data?.writing_prompts.map(
-            (suggestion: SuggestionProps) => ({
-              ...suggestion,
-              id: generateGuid(),
-            }),
-          );
-          setSuggestions(suggestionFormatted); //? IMPORTANT
-        }
-      } catch (error) {
-        console.error('Failed to parse the JSON string:', error);
-      }
-    } else {
-      console.error('Could not find the JSON content in the string.');
+    const data = extractJsonFromString<WritingPrompts>(text);
+    let suggestionFormatted = [];
+    if (data?.writing_prompts && data.writing_prompts.length > 0) {
+      suggestionFormatted = data?.writing_prompts.map(
+        (suggestion: SuggestionProps) => ({
+          ...suggestion,
+          id: generateGuid(),
+        }),
+      );
+      setSuggestions(suggestionFormatted);
     }
     return text;
   }
--- src/types/index.ts
+++ src/types/index.ts
@@ -10,3 +10,27 @@ export interface SuggestionCardProps {
   data: SuggestionProps;
   onChangeSuggestion: React.Dispatch<React.SetStateAction<string | null>>;
 }
+export interface WritingPrompts {
+  writing_prompts: SuggestionProps[];
+}
+export interface RevisionResponse {
+  overall_summary: string;
+  detailed_corrections: DetailedCorrection[];
+  style_and_fluency_suggestions: string;
+  additional_analysis: AdditionalAnalysis;
+  overall_score: number;
+}
+
+export interface DetailedCorrection {
+  error_type: string;
+  original_text: string;
+  suggested_text: string;
+  explanation: string;
+  impact_level: string;
+}
+
+export interface AdditionalAnalysis {
+  most_common_mistakes: string;
+  suggested_advanced_vocabulary: string[];
+  overall_punctuation: number;
+}
